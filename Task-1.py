a925eba7ab55d2e83e15aef478a1d17bd29c807f
Om Sai Vasireddy <111980577+omsaivasireddy@users.noreply.github.com>
2023-06-29 20:30:32 +0530

    Start
    Importing the required python libraries and loading the data

---
+++ Task-1.py
@@ -0,0 +1,10 @@
+# Import the necessary python libraries
+import pandas as pa
+import numpy as nu
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+
+classify = pa.read_csv("Iris.csv")
+print(classify)
